5609adfb097d6b237b97f92c86b69735649f2a1e
Qasim Warraich <qasim.warraich@gmail.com>
2020-05-12 01:26:27 +0200

    Added Integer Max one liner.

---
+++ int_max/intmax.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+
+int max(int a, int b){ return  a > b ? a : b;}
+
+int main(){
+   printf("%d\n",max(79,69)); 
+    
+   return 0;
+}
+
