0547d5d3a1d3a36c9e4cd72edebca5d082d966ea
Qasim Warraich <qasim.warraich@gmail.com>
2020-05-21 22:33:16 +0200

    added modulo demonstration for C

---
+++ modulodemonstration/modulodemonstration.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+//The modulo function in C is actually a remainder function. 
+//For example:
+//  5 %  2 =  1 :because 2*2 = 4 and the remainder is 1.
+// -5 %  2 = -1 :because 2*-2 = -4 and the remainder is -1.
+// -5 % -2 = -1 :because -2*2 = -4 and the remainder is -1.
+//  5 % -2 =  1 :because -2*-2 = 4 and the remnainder is 1.  
+
+
+int main(){
+    printf("%d\n",5%2);
+    printf("%d\n",-5%2);
+    printf("%d\n",-5%-2);
+    printf("%d\n", 5%-2);
+    
+    return 0;
+}
+
