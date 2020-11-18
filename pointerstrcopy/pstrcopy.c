00a1721a00c847b84071b1c31c21e5239096ed6b
qasimwarraich <qasim.warraich@gmail.com>
2020-11-18 18:16:01 +0100

    Fixed embarrasing mistake of not assigning length variable in point string copy

--- pointerstrcopy/pstrcopy.c
+++ pointerstrcopy/pstrcopy.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
+/* This program copies a string by making use of pointers */
 
-
-void pstrcopy(char* ptr,int len){
+void pstrcopy(char* ptr,int len)
+{
     char target[len];
-    for(int j=0; j<= len; j++){
+    for(int j = 0; j <= len; j++){
         *(target +j) = *(ptr +j);
     }
     printf("\n\n");
     printf("Target Array\n");
-    for(int i =0; i<=len; i++){printf("%c",target[i]);}
+    for(int i = 0; i <= len; i++){printf("%c",target[i]);}
     printf("\n\n");
 }
 
 
-int main(){
-
+int main()
+{
     char s[] = "spam vitali";
-    int len;
+    int len = 0;
     printf("Source Array\n");
-    for(int i =0; s[i]!= '\0'; i++){printf("%c",s[i]);len++;}
-    printf(" length =%d\n",(len));
+    for(int i = 0; s[i] != '\0'; i++){printf("%c",s[i]);len++;}
+    printf(" length = %d\n",(len));
     pstrcopy(s,len);
     return 0;
 }
