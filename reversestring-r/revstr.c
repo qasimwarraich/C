22ae5a20c2b1db0654b5e641c44ca02bffffd195
qasimwarraich <qasim.warraich@gmail.com>
2020-11-20 00:22:33 +0100

    Some minor refactoring in recursive example of string reverse

--- reversestring-r/revstr.c
+++ reversestring-r/revstr.c
@@ -1,24 +1,27 @@
 #include <stdio.h> 
 
 
-void reverseString(char* s, int sSize){
-    //Base case: Size of Array atleast 2 elements.
-    if(sSize > 1){ 
-        // Standard swapping procedure using temp.
-        char t = s[0];
-        s[0] = s[sSize-1];
-        s[sSize-1] = t;
-        //Reduce size by 2 places, one for head one of tail.
-        //Move one memory space over in s to create new effective head.
-        reverseString(s+1,sSize-2); 
+void reverse_string(char* s, int size)
+{
+    /* Base case: Size of Array atleast 2 elements. */
+    if(size > 1){ 
+        /* Standard swapping procedure using temp variable. */
+        char temp = s[0];
+        s[0] = s[size-1];
+        s[size-1] = temp;
+
+        /* Reduce size by 2 places, one for head one of tail. 
+        Move one memory space over in s to create new effective head. */
+        reverse_string(s+1, size-2); 
     } 
 }
 
-int main(){
-    char s[4] = "skuu";
-    int size = 4;
-    reverseString(s,size);
-    for (int i = 0; i < size; i++)  printf("%c",s[i]);
-    printf("\n");
+int main()
+{
+    char s[10] = "SomeString";
+    int size = 10;
+    reverse_string(s,size);
+    for (int i = 0; i < size; i++) printf("%c",s[i]); printf("\n");
+
     return 0;
 }
