293a9689c8b436b95f9af3afa632da72453a7e7d
Qasim Warraich <qasim.warraich@gmail.com>
2022-02-28 11:00:36 +0100

    Some formatting to the make file generator

--- makefilegenerator/printedversion/makescript.c
+++ makefilegenerator/printedversion/makescript.c
@@ -1,10 +1,10 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
 /* This program generates a barebones Makefile to assist for compiling simple
  * C projects. It receives a filename either from stdin or as a command line
- * argument. This is then used to write a Makefile. 
+ * argument. This is then used to write a Makefile.
  */
 
 const int MAXLENGTH = 1000;
@@ -12,34 +12,31 @@ const int MAXLENGTH = 1000;
 void makefilescript(char* filename);
 bool inputsanitiser(char* string);
 
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     char filename[MAXLENGTH];
 
     /* No args, fall back to stdin */
-    if (argc == 1){
+    if (argc == 1) {
         printf("\n### MAKE FILE GENERATOR ###\n\n");
         printf("Enter File Name: \n");
         fgets(filename, MAXLENGTH, stdin);
 
         /* Handles no input case */
-        if (filename[0] == '\n'){
+        if (filename[0] == '\n') {
             printf("Error: Please supply a filename\n");
             return 1;
         }
-    }
-    else if (argc > 2){
+    } else if (argc > 2) {
         printf("Error: Too many arguments\n Please supply only one argument\n");
         return 1;
-    }
-    else {
+    } else {
         strcpy(filename, argv[1]);
     }
-    
+
     /* Handles file extentions */
     bool extention = inputsanitiser(filename);
-    if (extention != true){
+    if (extention != true) {
         printf("Note: No file extention found, '.c' appended.\n");
         strcat(filename, ".c");
     }
@@ -50,7 +47,7 @@ int main(int argc, char *argv[])
 
 void makefilescript(char filename[MAXLENGTH])
 {
-    FILE *output = fopen("Makefile","w");
+    FILE* output = fopen("Makefile", "w");
     fputs("CC = gcc\n", output);
     fputs("CFLAGS = -g -Wall -pedantic\n\n\n", output);
     fputs("all:\n", output);
@@ -59,25 +56,24 @@ void makefilescript(char filename[MAXLENGTH])
     fputs("\trm -f a.out", output);
 
     fclose(output);
-    printf("\nMakefile for %s generated!\n\n", filename); 
+    printf("\nMakefile for %s generated!\n\n", filename);
 }
 
 /* Searches supplied filename for the dot, and then compares the string form
  * that point vs the .c file extention. It only returns true when the specified
- * filename's extention is exactly ".c" 
+ * filename's extention is exactly ".c"
  */
 bool inputsanitiser(char* str)
 {
-
     /* Trick for removing trailing \n and extraneous chars */
     str[strcspn(str, "\n")] = 0;
     str[strcspn(str, " ")] = 0;
 
-    char* dot = strchr(str,'.');
-    if (dot != NULL){
-        if (strcmp(dot, ".c") == 0) return true;
+    char* dot = strchr(str, '.');
+    if (dot != NULL) {
+        if (strcmp(dot, ".c") == 0)
+            return true;
         return false;
-    }
-    else
+    } else
         return false;
 }
