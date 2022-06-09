5ae5072d3ade4274b609e20adc942a9fff38b162
qasimwarraich <qasim.warraich@gmail.com>
2022-06-10 00:16:33 +0200

    Make store passing example more explicit

--- store_passing_conditional/main.c
+++ store_passing_conditional/main.c
@@ -6,7 +6,11 @@ const int NINE = 9;
 int main()
 {
     int x = EIGHT;
-    if ((x = NINE)) {
+    if (1) {
+        printf("%d\n", x); // Prints 8
+    }
+
+    if ((x = NINE)) { // x is updated in the test condition!
         printf("%d\n", x); // Prints 9
     }
 }
